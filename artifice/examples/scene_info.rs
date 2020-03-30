7ff9f8687c080c3ea74e143ef4738c6357f63390
Alexandre Bléron <alex.bleron@gmail.com>
2020-03-30 22:22:03 +0200

    after reorg

--- artifice/examples/scene_info.rs
+++ artifice/examples/scene_info.rs
@@ -9,25 +9,3 @@ use std::rc::Rc;
 use winit::event::{Event, WindowEvent};
 use winit::event_loop::ControlFlow;
 use winit::window::WindowBuilder;
-
-fn main() {
-    pretty_env_logger::init();
-
-    let mut app = Application::new();
-
-    let mut events = MainEventLoop::new();
-    let mut open_docs_counter = Rc::new(Cell::new(0));
-    events.with_window_ctx(move |ctx| {
-        open_document_window(ctx, open_docs_counter.clone(), None);
-    });
-
-    // Dispatcher: closure that receives and processes actions of type A
-    // - can have multiple dispatchers?
-
-    // VS
-    //
-
-
-
-    events.run();
-}
