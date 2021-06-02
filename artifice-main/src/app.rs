9ac0a680c923645ab8b0532c621c98c2af33ad5f
Alexandre Bléron <alex.bleron@gmail.com>
2021-06-02 22:02:34 +0200

    druid prototype

--- artifice-main/src/app.rs
+++ artifice-main/src/app.rs
@@ -1,171 +1,363 @@
-use artifice::model::{Atom, Composition, NodeId, Node};
-use eframe::{egui, epi};
-use std::{fs::File, io::Write, ops::Deref};
-
-struct AddPropertyWindowState {
-    target: NodeId,
-    name: String,
-    type_id: String,
-}
-
-impl Default for AddPropertyWindowState {
-    fn default() -> Self {
-        AddPropertyWindowState {
-            target: Default::default(),
-            name: "".to_string(),
-            type_id: "".to_string(),
-        }
-    }
-}
-
-pub struct TemplateApp {
-    comp: Composition,
-    add_property_window: Option<AddPropertyWindowState>,
-}
-
-impl Default for TemplateApp {
-    fn default() -> Self {
-        TemplateApp {
-            comp: Composition::new(),
-            add_property_window: None,
-        }
-    }
-}
-
-pub enum AppAction {
-    RenameNode {
-        node: NodeId,
-        name: Atom,
+use anyhow::Context;
+use artifice::model::{make_unique_name, Atom, Network, Node};
+use druid::{
+    lens,
+    commands::{REDO, UNDO},
+    text::{
+        format::{Formatter, Validation, ValidationError},
+        Selection,
     },
-    RenameProperty {
-        node: NodeId,
-        property: Atom,
-        name: Atom,
+    widget::{
+        Button, Controller, CrossAxisAlignment, Flex, Label, List, MainAxisAlignment, TextBox,
+        ValueTextBox,
     },
+    AppDelegate, Command, Data, DelegateCtx, Env, Event, EventCtx, FileDialogOptions, FileInfo,
+    FileSpec, Handled, Insets, Lens, LensExt, LocalizedString, MenuDesc, MenuItem, SysMods, Target,
+    UpdateCtx, Widget, WidgetExt,
+};
+use std::{fs::File, path::Path, sync::Arc};
+use tracing::{error, info};
+
+/// Application state
+#[derive(Clone, Data)]
+pub struct AppData {
+    /// Network being edited
+    network: Network,
+
+    /// Path to the file being edited, empty if not saved yet
+    #[data(ignore)]
+    current_file_info: Option<FileInfo>,
 }
 
-impl TemplateApp {
-    fn show_node_ui(&mut self, node: &Node, ui: &mut egui::Ui, actions: &mut Vec<AppAction>)
-    {
-        let label = if node.name().is_empty() {
-            "<root>".to_string()
-        } else {
-            node.name().to_string()
-        };
-
-        egui::CollapsingHeader::new(label).show(ui, move |ui| {
-
-            let mut name_edit = node.name().to_string();
-
-            ui.heading("Properties:");
-            ui.label("Name:");
-            if ui.text_edit_singleline(&mut name_edit).changed() {
-                /*actions.push(AppAction::RenameNode {
-                    node: node,
-                    name: name_edit.into(),
-                })*/
-            }
-            ui.end_row();
-
-            for prop in node.properties() {
-                ui.label(prop.name().deref());
-                ui.label(
-                    egui::Label::new(format!("({})", prop.type_id().deref()))
-                        .italics()
-                        .small(),
-                );
-                ui.end_row();
-            }
-
-            for child in node.children() {
-                self.show_node_ui(child, ui, actions);
-                ui.end_row();
-            }
-
-            ui.separator();
-
-            if ui.button("Add property...").clicked() {
-                self.add_property_window = Some(AddPropertyWindowState {
-                    target: node_id,
-                    .. Default::default()
-                });
-            }
-
-            if ui.button("Add child...").clicked() {
-                self.comp.create_node(node_id, "node".into());
-            }
-        });
-        ui.end_row();
+impl AppData {
+    /// Creates a new, blank application state.
+    pub fn new() -> AppData {
+        AppData {
+            network: Network::new(),
+            current_file_info: None,
+        }
     }
 
-    fn show_add_property_ui(&mut self, ctx: &egui::CtxRef) {
-        let mut open = self.add_property_window.is_some();
-        egui::Window::new("Add property")
-            .open(&mut open)
-            .show(ctx, |ui| {
-                egui::Grid::new("Add property").show(ui, |ui| {
-                    if let Some(ref mut state) = self.add_property_window {
-                        ui.label("Name:");
-                        ui.text_edit_singleline(&mut state.name);
-                        ui.end_row();
-                        ui.label("Type:");
-                        ui.text_edit_singleline(&mut state.type_id);
-                        ui.end_row();
-                        if ui.button("Add").clicked() {
-                            self.comp
-                                .node_mut(state.target)
-                                .unwrap()
-                                .add_property(Atom::from(state.name.as_str()), Atom::from(state.type_id.as_str()));
-                        }
-                    }
-                });
-            });
-        if !open {
-            self.add_property_window = None;
+    /// Saves the current network to the specified file.
+    pub fn save(&self, path: &Path) -> anyhow::Result<()> {
+        let mut file = File::create(path).context("Failed to open file for saving")?;
+        let json = self.network.to_json();
+        serde_json::to_writer_pretty(&mut file, &json).context("failed to write JSON")?;
+        Ok(())
+    }
+
+    /// Returns a lens to the network.
+    pub fn network_lens() -> impl Lens<Self, Network> {
+        druid::lens!(Self, network)
+    }
+}
+
+const FILE_TYPES: &[FileSpec] = &[FileSpec::new("Artifice JSON", &["json"])];
+
+fn get_default_file_dialog_options() -> FileDialogOptions {
+    FileDialogOptions::new().allowed_types(FILE_TYPES.to_vec())
+}
+
+/// Undo/redo controller.
+pub struct UndoRedoController {
+    /// Stack of previous states: the top is the current state
+    stack: Vec<Network>,
+
+    /// Position in the stack, starting from the top (0 is last pushed state)
+    position: usize,
+}
+
+impl UndoRedoController {
+    pub fn new() -> UndoRedoController {
+        UndoRedoController {
+            stack: vec![Network::new()],
+            position: 0,
         }
     }
 }
 
-impl epi::App for TemplateApp {
-    fn name(&self) -> &str {
-        "artifice-name"
+/// Gives a name to the changes (actions) that a child widget performs on some data.
+pub struct ActionWrapper {
+    action_name: String,
+}
+
+impl ActionWrapper {
+    pub fn new(name: impl Into<String>) -> ActionWrapper {
+        ActionWrapper {
+            action_name: name.into(),
+        }
     }
+}
 
-    /// Called each time the UI needs repainting, which may be many times per second.
-    /// Put your widgets into a `SidePanel`, `TopPanel`, `CentralPanel`, `Window` or `Area`.
-    fn update(&mut self, ctx: &egui::CtxRef, frame: &mut epi::Frame<'_>) {
-        egui::CentralPanel::default().show(ctx, |ui| {
-            let root = self.comp.root_id();
+impl<T: Data, W: Widget<T>> Controller<T, W> for ActionWrapper {
+    fn update(&mut self, child: &mut W, ctx: &mut UpdateCtx, old_data: &T, data: &T, env: &Env) {
+        child.update(ctx, old_data, data, env);
+        if !old_data.same(data) {
+            // TODO expose that to the UI. via commands?
+            tracing::info!("Command: {}", self.action_name);
+        }
+    }
+}
 
-            let mut actions = Vec::new();
-            self.show_node_ui(root, ui, &mut actions);
-            self.show_add_property_ui(ctx);
-
-            if ui.button("Save to JSON...").clicked() {
-                if let Ok(Some(path)) = native_dialog::FileDialog::new().show_save_single_file() {
-                    let json = self.comp.to_json();
-                    let mut file = File::create(path).unwrap();
-                    file.write(json.to_string().as_ref());
+impl<W: Widget<Network>> Controller<Network, W> for UndoRedoController {
+    fn event(
+        &mut self,
+        child: &mut W,
+        ctx: &mut EventCtx,
+        event: &Event,
+        data: &mut Network,
+        env: &Env,
+    ) {
+        // handle undo and redo commands
+        match event {
+            druid::Event::Command(cmd) if cmd.is(UNDO) => {
+                if self.position == 0 {
+                    tracing::warn!("nothing to undo");
+                } else {
+                    self.position -= 1;
+                    *data = self.stack[self.position].clone();
                 }
             }
+            druid::Event::Command(cmd) if cmd.is(REDO) => {
+                if self.position == self.stack.len() - 1 {
+                    // already at the top of the stack
+                    tracing::warn!("nothing to redo");
+                } else {
+                    self.position += 1;
+                    *data = self.stack[self.position].clone();
+                }
+            }
+            _ => {
+                child.event(ctx, event, data, env);
+            }
+        }
+    }
 
-            /*// The central panel the region left after adding TopPanel's and SidePanel's
-            ui.heading("egui template");
-            ui.hyperlink("https://github.com/emilk/egui_template");
-            ui.add(egui::github_link_file!(
-                "https://github.com/emilk/egui_template/blob/master/",
-                "Source code."
-            ));
-            egui::warn_if_debug_build(ui);*/
-        });
+    fn update(
+        &mut self,
+        child: &mut W,
+        ctx: &mut UpdateCtx,
+        old_data: &Network,
+        data: &Network,
+        env: &Env,
+    ) {
+        child.update(ctx, old_data, data, env);
 
-        /* if false {
-            egui::Window::new("Window").show(ctx, |ui| {
-                ui.label("Windows can be moved by dragging them.");
-                ui.label("They are automatically sized based on contents.");
-                ui.label("You can turn on resizing and scrolling if you like.");
-                ui.label("You would normally chose either panels OR windows.");
-            });
+        // If the data has changed, record the old data in the undo stack
+        if !old_data.same(data) && !self.stack[self.position].same(data) {
+            tracing::trace!("pushing undo entry");
+            // destroy edits after this one
+            self.stack.drain((self.position + 1)..);
+            self.stack.push(data.clone());
+            self.position = self.stack.len() - 1;
+        }
+    }
+}
+
+/// Application delegate.
+pub struct Delegate;
+
+impl AppDelegate<AppData> for Delegate {
+    fn command(
+        &mut self,
+        ctx: &mut DelegateCtx,
+        target: Target,
+        cmd: &Command,
+        data: &mut AppData,
+        _env: &Env,
+    ) -> Handled {
+        if cmd.is(druid::commands::SAVE_FILE_AS) {
+            let file_info = cmd.get_unchecked(druid::commands::SAVE_FILE_AS);
+            match data.save(file_info.path()) {
+                Ok(_) => {
+                    // save success, update save path
+                    data.current_file_info = Some(file_info.clone());
+                    info!("saved to {}", file_info.path().display());
+                }
+                Err(e) => {
+                    // TODO message box instead?
+                    error!("error saving file: {:?}", e);
+                }
+            }
+            Handled::Yes
+        } else if cmd.is(druid::commands::SAVE_FILE) {
+            if let Some(ref file_info) = data.current_file_info {
+                // save to the current path
+                match data.save(file_info.path()) {
+                    Ok(_) => {
+                        info!("saved to {}", file_info.path().display());
+                    }
+                    Err(e) => {
+                        // TODO message box instead?
+                        error!("error saving file: {:?}", e);
+                    }
+                }
+            } else {
+                // not saved, show the file save dialog
+                ctx.submit_command(
+                    druid::commands::SHOW_SAVE_PANEL
+                        .with(get_default_file_dialog_options())
+                        .to(target),
+                );
+            }
+
+            //data.save_to_json().unwrap();
+            Handled::Yes
+        } else {
+            println!("cmd forwarded: {:?}", cmd);
+            Handled::No
+        }
+    }
+}
+
+type NodeList = Arc<Vec<Node>>;
+
+/// Formatter that automatically renames the inputted name if it clashes with another node.
+struct NodeIdentFormatter;
+
+impl Formatter<Atom> for NodeIdentFormatter {
+    fn format(&self, name: &Atom) -> String {
+        name.to_string()
+    }
+
+    fn validate_partial_input(&self, input: &str, _sel: &Selection) -> Validation {
+        Validation::success()
+        /*if let Some(unique_name) = make_unique_name(input, self.siblings.iter().map(|n| n.name())) {
+            Validation::success().change_text(unique_name)
+        } else {
+            Validation::success()
         }*/
     }
+
+    fn value(&self, input: &str) -> Result<Atom, ValidationError> {
+        Ok(Atom::from(input))
+    }
+}
+
+/// Controller in charge of ensuring that the name of a node is unique within a list of siblings.
+struct NodeRenameController;
+
+impl<W: Widget<(NodeList, Node)>> Controller<(NodeList, Node), W> for NodeRenameController {
+    fn event(
+        &mut self,
+        child: &mut W,
+        ctx: &mut EventCtx,
+        event: &Event,
+        data: &mut (NodeList, Node),
+        env: &Env,
+    ) {
+        let old = data.1.clone();
+        child.event(ctx, event, data, env);
+        if !old.same(&data.1) {
+            data.1.name = make_unique_name(data.1.name.clone(), data.0.iter().map(|n| &n.name));
+        }
+    }
+}
+
+pub fn node_ui(depth: u32) -> impl Widget<(NodeList, Node)> {
+    let indent = Insets::new((depth * 10) as f64, 0.0, 0.0, 0.0);
+
+    let mut vbox = Flex::column().cross_axis_alignment(CrossAxisAlignment::Start);
+
+    // name row
+    vbox.add_child(
+        Flex::row()
+            //.must_fill_main_axis(true)
+            .with_child(Label::new("Name").padding(indent).fix_width(200.0))
+            .with_flex_child(
+                ValueTextBox::new(TextBox::new(), NodeIdentFormatter)
+                    .lens(lens!(Node, name))
+                    .lens(lens!((NodeList, Node), 1))
+                    .controller(NodeRenameController)
+                    .controller(ActionWrapper::new("rename node"))
+                    .expand_width(),
+                1.0,
+            ),
+    );
+
+    // button to add a child
+    vbox.add_child(
+        Button::new("Add child")
+            .on_click(|_, data: &mut (NodeList, Node), _| {
+                data.1.add_child("node".into());
+            })
+            .padding(indent),
+    );
+
+    // children
+    vbox.add_child(
+        List::new(move || node_ui(depth + 1)).lens(druid::lens::Identity.map(
+            |(_, node): &(NodeList, Node)| (node.children.clone(), node.children.clone()),
+            |(_, node): &mut (NodeList, Node), (_, new): (NodeList, NodeList)| {
+                node.children = new;
+            },
+        )),
+    );
+
+    vbox
+}
+
+pub fn ui() -> impl Widget<AppData> {
+    let mut root = Flex::column();
+
+    root.add_child(
+        node_ui(0)
+            .lens(druid::lens::Identity.map(
+                |net: &Network| (Arc::new(Vec::new()), net.root().clone()),
+                |net: &mut Network, (_, new): (_, Node)| {
+                    net.root = new;
+                }
+            ))
+            .controller(UndoRedoController::new())
+            .lens(AppData::network_lens())
+            .fix_width(600.0),
+    );
+    root
+    //root.debug_paint_layout()
+}
+
+pub fn application_menu() -> MenuDesc<AppData> {
+    MenuDesc::new(LocalizedString::new("artifice").with_placeholder("artifice"))
+        .append(
+            MenuDesc::new(LocalizedString::new("artifice.file-menu").with_placeholder("File"))
+                .append(
+                    MenuItem::new(
+                        LocalizedString::new("artifice.open").with_placeholder("Open..."),
+                        druid::commands::SHOW_OPEN_PANEL.with(get_default_file_dialog_options()),
+                    )
+                    .hotkey(SysMods::Cmd, "o"),
+                )
+                .append(
+                    MenuItem::new(
+                        LocalizedString::new("artifice.save").with_placeholder("Save"),
+                        druid::commands::SAVE_FILE,
+                    )
+                    .hotkey(SysMods::Cmd, "s"),
+                )
+                .append(
+                    MenuItem::new(
+                        LocalizedString::new("artifice.save-as").with_placeholder("Save as..."),
+                        druid::commands::SHOW_SAVE_PANEL.with(get_default_file_dialog_options()),
+                    )
+                    .hotkey(SysMods::CmdShift, "s"),
+                )
+                .append_separator()
+                .append(MenuItem::new(
+                    LocalizedString::new("artifice.quit").with_placeholder("Quit"),
+                    druid::commands::QUIT_APP,
+                )),
+        )
+        .append(
+            MenuDesc::new(LocalizedString::new("artifice.edit-menu").with_placeholder("Edit"))
+                .append(druid::platform_menus::common::undo())
+                .append(druid::platform_menus::common::redo())
+                .append_separator()
+                .append(druid::platform_menus::common::cut())
+                .append(druid::platform_menus::common::copy())
+                .append(druid::platform_menus::common::paste())
+                .append(MenuItem::new(
+                    LocalizedString::new("artifice.redo").with_placeholder("Redo"),
+                    druid::commands::REDO,
+                )),
+        )
 }
--- artifice/src/model/mod.rs
+++ artifice/src/model/mod.rs
@@ -1,56 +1,76 @@
-mod edit;
-
-use string_cache::DefaultAtom;
-use std::ops::Deref;
-use std::cell::{Cell, RefCell, Ref, RefMut};
 use serde_json::json;
-use std::rc::Rc;
-use std::sync::Arc;
+use std::{fmt, fmt::Formatter, ops::Deref, sync::Arc};
+use string_cache::DefaultAtom;
+use druid::Lens;
 
-// Want something with history?
-// -> Revision<T>
-// - immutable, clonable
-// - internally:
-//      - Arc<Log>
-//      - Revision in the history stack
-//      - "synthesized" value (== checkout) (optional)
-// - comparison is cheap, however getting the value of a revision may not be (if it's not checked out)
+/// Atom (interned string used for names)
+#[derive(Clone, Debug, Eq, PartialEq, Default, serde::Serialize)]
+pub struct Atom(DefaultAtom);
 
-pub type Atom = DefaultAtom;
-
-#[derive(Clone,Debug)]
-pub struct Node {
-    /// Name of this node
-    name: Atom,
-
-    /// Properties
-    properties: Arc<Vec<Property>>,
-
-    /// Child nodes
-    children: Arc<Vec<Node>>,
+impl Deref for Atom {
+    type Target = DefaultAtom;
+    fn deref(&self) -> &Self::Target {
+        &self.0
+    }
 }
 
+impl druid::Data for Atom {
+    fn same(&self, other: &Self) -> bool {
+        &self.0 == &other.0
+    }
+}
+
+impl<T> From<T> for Atom
+where
+    DefaultAtom: From<T>,
+{
+    fn from(value: T) -> Self {
+        Atom(DefaultAtom::from(value))
+    }
+}
+
+impl fmt::Display for Atom {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        self.0.fmt(f)
+    }
+}
 
 /// Helper function to adjust a name so that it doesn't clash with existing names.
-fn make_unique_name<'a>(base_name: Atom, existing: impl Iterator<Item=&'a Atom> + Clone) -> Atom {
-    let mut unique_name = base_name.to_string();
+pub fn make_unique_name<'a>(base_name: Atom, existing: impl Iterator<Item = &'a Atom> + Clone) -> Atom {
     let mut counter = 0;
+    let mut disambiguated_name = base_name.clone();
 
     'check: loop {
         let existing = existing.clone();
         // check for property with the same name
         for name in existing {
-            if name.deref() == &unique_name {
-                unique_name = format!("{}_{}", base_name.deref(), counter);
+            if name == &disambiguated_name {
+                disambiguated_name = Atom::from(format!("{}_{}", base_name, counter));
                 counter += 1;
                 // restart check
-                continue 'check
+                continue 'check;
             }
         }
-        break
+        break;
     }
 
-    Atom::from(unique_name)
+
+    disambiguated_name
+}
+
+/// Nodes
+#[derive(Clone, Debug, druid::Data, druid::Lens)]
+pub struct Node {
+    /// Name of this node
+    //#[lens(name = "name_lens")]
+    pub name: Atom,
+
+    /// Properties
+    #[lens(name = "properties_lens")]
+    pub properties: Arc<Vec<Property>>,
+
+    /// Child nodes
+    pub children: Arc<Vec<Node>>,
 }
 
 impl Node {
@@ -59,54 +79,59 @@ impl Node {
         Node {
             name: Default::default(),
             properties: Default::default(),
-            children: Default::default()
+            children: Default::default(),
         }
     }
 
-    /// Returns the name of this node.
-    pub fn name(&self) -> Atom {
-        self.0.borrow().name.clone()
+    /// Returns an iterator over the properties of this node.
+    pub fn properties(&self) -> impl Iterator<Item = &Property> {
+        self.properties.iter()
     }
 
-    /// Returns the child nodes.
-    pub fn children(&self) -> &[Node] {
-        &self.children
+    /// Creates a new child node
+    pub fn add_child(&mut self, name: Atom) {
+        let unique_name = make_unique_name(name, self.children.iter().map(|p| &p.name));
+        Arc::make_mut(&mut self.children).push(Node {
+            name: Atom::from(unique_name),
+            properties: Arc::new(vec![]),
+            children: Arc::new(vec![]),
+        })
     }
-}
 
-impl Default for Node {
-    fn default() -> Self {
-        Node::new()
+    /// Serializes the node to a JSON value.
+    pub fn to_json(&self) -> serde_json::Value {
+        let mut children_json = Vec::new();
+
+        for node in self.children.iter() {
+            children_json.push(node.to_json());
+        }
+
+        json!({
+            "name": &self.name,
+            //"properties": &node.properties,
+            "children": children_json,
+        })
     }
-}
-
-impl Node {
 
     /// Adds a property to this node
-    pub fn add_property(
-        &mut self,
-        name: Atom,
-        ty: Atom
-    ) -> Atom
-    {
-        let unique_name = make_unique_name(name, inner.properties.iter().map(|p| &p.name));
-        
+    pub fn add_property(&mut self, name: Atom, ty: Atom) -> Atom {
+        let unique_name = make_unique_name(name, self.properties.iter().map(|p| &p.name));
+
         Arc::make_mut(&mut self.properties).push(Property {
             name: unique_name.clone(),
             ty,
-            value: Default::default()
+            value: Default::default(),
         });
 
         unique_name
     }
 
-    pub fn properties(&self) -> impl Iterator<Item=&Property> {
-        self.properties.iter()
-    }
-
     /// Returns whether this node has a property with the given name.
     pub fn has_property(&self, name: &Atom) -> bool {
-        self.properties.iter().position(|p| &p.name == name).is_some()
+        self.properties
+            .iter()
+            .position(|p| &p.name == name)
+            .is_some()
     }
 
     /// Gets the property with the given name.
@@ -115,7 +140,7 @@ impl Node {
     }
 
     pub fn dump(&self, indent: usize) {
-        println!("{:indent$}name: {}", "", self.name, indent=indent);
+        println!("{:indent$}name: {}", "", self.name, indent = indent);
 
         {
             let indent = indent + 2;
@@ -126,103 +151,73 @@ impl Node {
     }
 }
 
-#[derive(Clone,Debug, serde::Serialize)]
+impl Default for Node {
+    fn default() -> Self {
+        Node::new()
+    }
+}
+
+/// Node property.
+#[derive(Clone, Debug, serde::Serialize, druid::Data, druid::Lens)]
 pub struct Property {
-    /// Name
+    /// Name of the property. Unique among all properties of a node.
+    #[lens(name = "name_lens")]
     name: Atom,
 
     /// Type identifier
+    #[lens(name = "ty_lens")]
     ty: Atom,
 
-    /// Value of the property.
+    /// Value of the property. It may be null if
+    #[lens(name = "value_lens")]
+    #[data(same_fn = "PartialEq::eq")]
     value: serde_json::Value,
 }
 
 impl Property {
+    /// Name of the property
     pub fn name(&self) -> &Atom {
         &self.name
     }
 
+    /// Type ID of the property
     pub fn type_id(&self) -> &Atom {
         &self.ty
     }
 
     pub fn dump(&self, indent: usize) {
-        println!("{:indent$}name  : {}", "", self.name, indent=indent);
-        println!("{:indent$}type  : {}", "", self.ty, indent=indent);
-        println!("{:indent$}value : {:?}", "", self.value, indent=indent);
+        println!("{:indent$}name  : {}", "", self.name, indent = indent);
+        println!("{:indent$}type  : {}", "", self.ty, indent = indent);
+        println!("{:indent$}value : {:?}", "", self.value, indent = indent);
     }
 }
 
-#[derive(Clone)]
-pub struct Composition {
-    root: Arc<Node>,
+/// A hierarchical set of interconnected nodes.
+#[derive(Clone, druid::Data, druid::Lens)]
+pub struct Network {
+    #[lens(name = "root_lens")]
+    pub root: Node,
 }
 
-impl Composition {
-
-    /// Creates a new composition.
-    pub fn new() -> Composition {
-        let root = Arc::new(Node::new());
-        Composition {
-            root
-        }
+impl Network {
+    /// Creates a new, empty network.
+    pub fn new() -> Network {
+        let root = Node::new();
+        Network { root }
     }
 
-    pub fn root(&self) -> &Arc<Node> {
-        self.root
+    pub fn root(&self) -> &Node {
+        &self.root
     }
 
-    pub fn create_node(&mut self, parent: NodeId, name: Atom) -> NodeId {
-        let name = make_unique_name(name, self.nodes.values().map(|n| &n.name));
-        let id = self.nodes.insert( Node {
-            name,
-            properties: vec![],
-            children: vec![]
-        });
-        let parent = self.nodes.get_mut(parent).unwrap();
-        parent.children.push(id);
-        id
-    }
-
-    pub fn from_json(json: serde_json::Value) -> Result<Composition, anyhow::Error> {
+    /*pub fn from_json(json: serde_json::Value) -> Result<Composition, anyhow::Error> {
         let mut comp = Composition::new();
         comp.merge_from_json(json);
         Ok(comp)
-    }
+    }*/
 
-    pub fn merge_from_json(&mut self, json: serde_json::Value) {
-        todo!()
-    }
-
-    pub fn node(&self, id: NodeId) -> Option<&Node> {
-        self.nodes.get(id)
-    }
-
-    pub fn node_mut(&mut self, id: NodeId) -> Option<&mut Node> {
-        self.nodes.get_mut(id)
-    }
-
-    pub fn nodes(&mut self) -> impl Iterator<Item=(NodeId, &Node)> {
-        self.nodes.iter()
-    }
-
-    fn node_to_json_recursive(&self, node: &Node) -> serde_json::Value {
-        let mut children = Vec::new();
-        for &id in node.children.iter() {
-            let node = self.node(id).unwrap();
-            children.push(self.node_to_json_recursive(node));
-        }
-
-        json!({
-            "name": &node.name,
-            "properties": &node.properties,
-            "children": children,
-        })
-    }
-
+    /// Serializes this node to JSON.
     pub fn to_json(&self) -> serde_json::Value {
-        let json = self.node_to_json_recursive(self.nodes.get(self.root).unwrap());
-        json
+        self.root.to_json()
     }
 }
